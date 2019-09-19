d6630d01007181af4693cf94e6adc93fdee0fa06
Chris Zhu <czhu4@saic.edu>
2019-09-18 22:49:53 -0500

    stick figures1

--- scripts.js
+++ scripts.js
@@ -401,3 +401,20 @@ function scrollAnchors(e, respond = null) {
 		}
 	}, 100);
 }
+
+
+/* navbar stuff here */
+
+function openNav(evt, navName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("navtab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(navName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
